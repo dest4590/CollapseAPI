88935c9bda792deb54ab2c7d27b8419d2f9bb6ca
dest4590 <dest4590@gmail.com>
2025-06-18 15:35:39 +0300

    fix: update loader launch endpoint to remove loader_id from URL

--- CollapseAPI/urls.py
+++ CollapseAPI/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path(
         "api/client/<int:client_id>/download", client_download, name="client_download"
     ),
-    path("api/loader/<int:loader_id>/launch", loader_launch, name="loader_launch"),
+    path("api/loader/launch", loader_launch, name="loader_launch"),
     path("admin/", admin.site.urls),
     # SWAG $$$
     path(

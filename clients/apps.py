d78586d7ccaa39709f85c4311c69b722483737fc
Arsenii <ttfdk@icloud.com>
2025-07-08 04:05:57 +0300

    fix: fixed bug causing heartbeat to be sent two times

--- clients/apps.py
+++ clients/apps.py
@@ -1,10 +1,11 @@
 from django.apps import AppConfig
-
+import os
 
 class ClientsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "clients"
 
     def ready(self):
-        from . import heartbeat
-        heartbeat.start()
+        if os.environ.get("RUN_MAIN") == "true":
+            from . import heartbeat
+            heartbeat.start()
